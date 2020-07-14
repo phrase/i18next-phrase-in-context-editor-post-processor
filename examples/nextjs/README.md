17cfc88475086e86974e57fac44203d81e782563
Jakub Freisler <jakubfreisler@phrase.com>
2020-07-14 10:11:15 +0200

    chore: add next.js example project
    add README to the example

--- examples/nextjs/README.md
+++ examples/nextjs/README.md
@@ -1,4 +1,4 @@
-# i18next Phrase In-Context Editor post processor react demo
+# i18next Phrase In-Context Editor post processor nextjs demo
 
 ## How to use the demo
 
@@ -6,13 +6,13 @@
 
 ```bash
 yarn
-yarn start
+yarn dev
 ```
 
 New tab in your default browser should open. If that's not the case look into your console for the development server address (by default, it's [http://localhost:3000](http://localhost:3000).
 
 The page will reload if you make edits.
-Any lint errors will be visible in the console.
+Any nextjs errors will be visible within console.
 
 ### Login to Phrase demo project
 
@@ -26,3 +26,7 @@ Password: phrase
 ## More information
 
 For more info, please have a look at [i18next Phrase In-Context Editor post processor readme](https://github.com/phrase/i18next-phrase-in-context-editor-post-processor).
+
+### Notice
+
+This demo was built on top of the starter template from [Learn Next.js guide](https://nextjs.org/learn) and the [next-i18next simple example project](https://github.com/isaachinman/next-i18next/tree/master/examples/simple).
---
+++ examples/nextjs/pages/index.js
@@ -0,0 +1,110 @@
+import Head from 'next/head';
+import PropTypes from 'prop-types';
+
+import { i18n, withTranslation } from '../i18n';
+
+function Home({ t }) {
+    return (
+        <div className="container">
+            <Head>
+                <title>Create Next App</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <main>
+                <h1 className="title">
+                    {t('title')}
+                </h1>
+
+                <p className="description">
+                    {t('intro')}
+                </p>
+
+                <button
+                    type='button'
+                    onClick={() => i18n.changeLanguage(i18n.language === 'en' ? 'de' : 'en')}
+                >
+                    {t('change-locale')}
+                </button>
+
+            </main>
+
+            <style jsx>{`
+        .container {
+          min-height: 100vh;
+          padding: 0 0.5rem;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+        a {
+          color: inherit;
+          text-decoration: none;
+        }
+
+        .title a {
+          color: #0070f3;
+          text-decoration: none;
+        }
+
+        .title a:hover,
+        .title a:focus,
+        .title a:active {
+          text-decoration: underline;
+        }
+
+        .title {
+          margin: 0;
+          line-height: 1.15;
+          font-size: 4rem;
+        }
+
+        .title,
+        .description {
+          text-align: center;
+        }
+
+        .description {
+          line-height: 1.5;
+          font-size: 1.5rem;
+        }
+      `}</style>
+
+            <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+        </div>
+    );
+}
+
+Home.getInitialProps = () => Promise.resolve({
+    namespacesRequired: ['common'],
+});
+
+Home.propTypes = {
+    t: PropTypes.func.isRequired,
+};
+
+export default withTranslation('common')(Home);
