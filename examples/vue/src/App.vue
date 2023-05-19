46e99999a3b590c99143cc713a6d0b456116cb4e
Varpusparvi <mikko.karkee1@gmail.com>
2023-05-19 12:02:53 +0200

    Update vue example to newest versions

--- examples/vue/src/App.vue
+++ examples/vue/src/App.vue
@@ -5,16 +5,8 @@
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
-
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class App extends Vue {}
+<script lang="ts" setup>
+import HelloWorld from '@/components/HelloWorld.vue';
 </script>
 
 <style>
--- examples/vue/src/i18n.ts
+++ examples/vue/src/i18n.ts
@@ -1,46 +1,23 @@
 
-import Vue from 'vue';
-import i18next from 'i18next';
-import VueI18Next from '@panter/vue-i18next';
-import PhraseInContextEditorPostProcessor from 'i18next-phrase-in-context-editor-post-processor';
 
-// the translations
-// (tip move them in a JSON file and import them)
-const resources = {
-    en: {
-        translation: {
-            'title': 'This headline should be replaced when In-Context editor is enabled',
-            'intro': 'Intro translation',
-        }
-    },
-    de: {
-        translation: {
-            'title': 'Hallo',
-            'intro': 'Test',
-        }
-    }
+import i18next from "i18next";
+import PhraseInContextEditorPostProcessor from "i18next-phrase-in-context-editor-post-processor";
+import { ref } from "vue";
+
+export const i18nextInstance = i18next.createInstance({
+    postProcess: ['phraseInContextEditor']
+});
+
+export const initializeI18next = async () => {
+    i18nextInstance
+        .use(new PhraseInContextEditorPostProcessor({
+            phraseEnabled: true,
+            projectId: '00000000000000004158e0858d2fa45c',
+            accountId: '0bed59e5',
+            useOldICE: false,
+        }));
+    await i18nextInstance.init();
 };
-
-Vue.use(VueI18Next);
-
-// eslint-disable-next-line @typescript-eslint/no-floating-promises
-i18next
-    .use(new PhraseInContextEditorPostProcessor({
-        phraseEnabled: true,
-        projectId: '00000000000000004158e0858d2fa45c',
-        accountId: '0bed59e5',
-        useOldICE: false,
-    }))
-    .init({
-        resources,
-        lng: 'en',
-        postProcess: ['phraseInContextEditor']
-    });
-
-declare module 'vue/types/options' {
-    interface ComponentOptions<V extends Vue> {
-        i18n?: VueI18Next;
-    }
-}
-
-export default new VueI18Next(i18next);
+export const useTranslate = () => {
+    return {t:ref(i18nextInstance.t.bind(i18nextInstance))};
+};
