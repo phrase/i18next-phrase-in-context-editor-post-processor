a2e72b4a5acf394a7bc5d4b4a8c95abe019cdae7
Varpuspaavi <mikko.karkee1@gmail.com>
2026-01-15 12:34:51 +0100

    Add example for Trans elements for react

--- examples/react/src/index.tsx
+++ examples/react/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import i18n from "i18next";
-import { useTranslation, initReactI18next } from "react-i18next";
+import { useTranslation, initReactI18next, Trans } from "react-i18next";
 import PhraseInContextEditorPostProcessor from "i18next-phrase-in-context-editor-post-processor";
 
 await i18n
@@ -11,8 +11,8 @@ await i18n
         projectId: '00000000000000004158e0858d2fa45c',
         accountId: '0bed59e5',
         useOldICE: false,
-        prefix: '{{__phrase_',
-        suffix: '__}}',
+        prefix: '[[__phrase_', // This can't be default "{{__phrase_" to make Trans elements work
+        suffix: '__]]', // {{}} strings don't play nicely with React
     }))
     .init({
         resources: {
@@ -30,7 +30,32 @@ await i18n
 
 function App() {
     const { t } = useTranslation();
-    return <h2>{t('some_test_key_name')}</h2>;
+    const count = 1;
+    return <h2>
+        {t('some_test_key_name')}
+        <br />
+        <Trans
+            i18nKey="my.key"
+            // disable post processing for this Trans call
+            tOptions={{ postProcess: false }}
+            components={{ strong: <strong /> }}
+        />
+        <br />
+        <Trans t={t}>Hello World</Trans>
+        <br />
+        <Trans i18nKey="pages.nlp.conversationInsights.expressionsDrawer.description" tOptions={{postProcess: ['phraseInContextEditor']}}/>
+        <br />
+        <Trans i18nKey="userMessagesUnread" count={count}>
+            Hello <strong title={t('nameTitle')}>{{name}}</strong>, you have {{count}} unread message.
+        </Trans>
+        <br />
+        <Trans
+            i18nKey="myKey" // optional -> fallbacks to defaults if not provided
+            defaults="hello <italic>beautiful</italic> <bold>{{what}}</bold>" // optional defaultValue
+            values={{ what: 'world'}}
+            components={{ italic: <i />, bold: <strong /> }}
+        />
+    </h2>;
 }
 
 // append app to dom
