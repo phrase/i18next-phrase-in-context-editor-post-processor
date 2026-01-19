a016c50968e7d48f4c090cb7e3094f91d1480cb4
Varpuspaavi <mikko.karkee1@gmail.com>
2026-01-19 11:58:31 +0100

    fix: use direct npm release

--- release.config.js
+++ release.config.js
@@ -3,8 +3,9 @@ module.exports = {
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
         "@semantic-release/changelog",
-        ["@semantic-release/npm", {
-            "tarballDir": "release"
+        ["@semantic-release/exec", {
+            prepareCmd: "npm version ${nextRelease.version} --no-git-tag-version",
+            publishCmd: "npm publish --provenance"
         }],
         "@semantic-release/git"
     ],
